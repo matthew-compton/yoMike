11714fd0daaad004c1ad122ff1f1ed681a78fb11
matthew-compton <compton@bignerdranch.com>
2015-03-03 00:16:44 -0500

    I have no goals or aims

--- app/src/main/java/com/example/tomk/yomike/yoActivity.java
+++ app/src/main/java/com/example/tomk/yomike/yoActivity.java
@@ -1,72 +1,54 @@
 package com.example.tomk.yomike;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.content.pm.ActivityInfo;
-import android.content.ComponentName;
-import android.content.Context;
-import java.util.List;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-public class yoActivity extends ActionBarActivity {
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class YoActivity extends Activity {
+
+    private static final String PACKAGE_NAME_TWITTER = "com.twitter.android.composer.ComposerActivity";
+
+    @InjectView(R.id.activity_yo_image) ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yo);
+        ButterKnife.inject(this);
+        mImageView.setOnClickListener(mOnClickListener);
+    }
 
-        final Button button = (Button) findViewById(R.id.yoMikeButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent shareIntent = new Intent();
-                shareIntent.setAction(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, "Yo, @m_evans10 !");
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.yo));
 
-                final PackageManager pm = v.getContext().getPackageManager();
-                final List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
-                for (final ResolveInfo app : activityList) {
-
-                    if ("com.twitter.android.composer.ComposerActivity".equals(app.activityInfo.name)) {
-                        final ActivityInfo activity = app.activityInfo;
-                        final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
-                        shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-                        shareIntent.setComponent(name);
-                        v.getContext().startActivity(shareIntent);
-                        break;
-                    }
+            final PackageManager pm = v.getContext().getPackageManager();
+            final List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+            for (final ResolveInfo app : activityList) {
+                if (PACKAGE_NAME_TWITTER.equals(app.activityInfo.name)) {
+                    final ActivityInfo activityInfo = app.activityInfo;
+                    final ComponentName componentName = new ComponentName(activityInfo.applicationInfo.packageName, activityInfo.name);
+                    shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    shareIntent.setComponent(componentName);
+                    startActivity(shareIntent);
+                    finish();
                 }
             }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_yo, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+    };
 
-        return super.onOptionsItemSelected(item);
-    }
 }
